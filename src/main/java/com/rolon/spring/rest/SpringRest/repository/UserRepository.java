e0f72ffcf50a57351a648b1f21c712d10b10679a
JesusRRr <rolonjesus1996@gmail.com>
2020-06-29 16:14:58 -0500

    feature: UserREpositoty created

---
+++ src/main/java/com/rolon/spring/rest/SpringRest/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.rolon.spring.rest.SpringRest.repository
+;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rolon.spring.rest.SpringRest.model.UserModel;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserModel, Integer>{
+
+}

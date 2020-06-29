2a084ece99452842007761ba906137729bb92037
JesusRRr <rolonjesus1996@gmail.com>
2020-06-29 16:43:23 -0500

    chore: getUsers and saveUser methods created in UserController class

--- src/main/java/com/rolon/spring/rest/SpringRest/controller/UserController.java
+++ src/main/java/com/rolon/spring/rest/SpringRest/controller/UserController.java
@@ -1,9 +1,15 @@
 package com.rolon.spring.rest.SpringRest.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.rolon.spring.rest.SpringRest.model.UserModel;
 import com.rolon.spring.rest.SpringRest.service.UserService;
 
 @RestController
@@ -11,6 +17,15 @@ import com.rolon.spring.rest.SpringRest.service.UserService;
 public class UserController {
 	@Autowired
 	UserService userService;
-
+	
+	@GetMapping()
+	public List<UserModel> getUsers(){
+		return userService.getUsers();
+	}
+	
+	@PostMapping()
+	public UserModel saveUser(@RequestBody UserModel user){
+		return userService.saveUser(user);
+	}
 	
 }

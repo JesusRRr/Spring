f7e779090bfb31f5d8394c3911d634c51dc4278b
JesusRRr <rolonjesus1996@gmail.com>
2020-06-29 16:20:52 -0500

    feature: UserService class created

---
+++ src/main/java/com/rolon/spring/rest/SpringRest/service/UserService.java
@@ -0,0 +1,18 @@
+package com.rolon.spring.rest.SpringRest.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rolon.spring.rest.SpringRest.model.UserModel;
+import com.rolon.spring.rest.SpringRest.repository.UserRepository;
+
+@Service
+public class UserService{
+	@Autowired
+	UserRepository userRepository;
+
+	
+	
+}
